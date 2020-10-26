1338f3e5a2cc9486949fae6c5060d3348df01266
Dev-hottae <hottae0805@gmail.com>
2020-10-27 00:13:27 +0900

    before server test

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import os
+from tokenizers import CharBPETokenizer, ByteLevelBPETokenizer, SentencePieceBPETokenizer, BertWordPieceTokenizer
 
 ### 전체 프로세스
 
@@ -6,12 +9,57 @@
 
 ## 크롤링 종료 후 데이터 전처리
 from Data_crawler.data_pre_processing import Pre
+from topic_modeling.tokenizer import Tokenizer
 
-path = r"C:\Users\dlagh\Desktop\final_project"
-pre = Pre(keyword="삼성전자", path=path)
-pre.run_processing()
+path = r"Data_crawler/dataset"
+# pre = Pre(keyword="삼성전자", path=path)
+# pre.run_processing()
+print("전처리 완료")
 
 ## TOPIC 모델링을 통한 1일 핵심 주제 키워드 추출
+# data 토크나이저
+# data load
+print("데이터 로딩 중")
+data_dir = 'Data_crawler/dataset/삼성전자_pred'
+data_list = os.listdir(data_dir)
+data = pd.DataFrame([])
+for data_file in data_list:
+    news = pd.read_json(data_dir+"/"+data_file, encoding="utf-8")
+    data = data.append(news)
+print("데이터 로딩 완료")
+print("============")
 
+print("데이터 토크나이징")
+tok = Tokenizer(data=data, vocab_file=None)
+# data 날짜 구간 조건
+tok.date_condition(start_date="2005-01-01")
+# # 데이터 맞춤법 교정 // 현재 속도 굉장히 느림
+print("맞춤법 교정중")
+tok.check_spell(data_trunc=20, max_char=500, dropna=True)
+print("맞춤법 교정완료")
+# mecab 을 활용한 불용어제거
+print("불용어제거중")
+tok.ex_stopword(True) ## 일단 mecab의 명사와 동사 추출
+print("불용어제거완료")
+# tokenizer train
+print("토크나이저 사전 구축중")
+tok.train_vocab(vocab_size=32000, min_freq=5, tokenizer=BertWordPieceTokenizer())
+print("토크나이저 사전 구축완료")
+
+# tokenize
+print("토크나이징")
+token_df = tok.tokenizer(tokenizer=BertWordPieceTokenizer(vocab_file='topic_modeling/huggingface_tokenizer_kor_32000-vocab.txt'))
+print("토크나이징 완료")
+
+# 데이터 1차 저장
+token_df.to_json("after_tokenizing.json")
+
+
+
+
+# LDA 토픽추출
+print("LDA run...")
+test = tok.get_lda(n=100)
+print("LDA 완료")
 
 ## 추출된 키워드를 키반 GPT 알고리즘을 통해 자연어 생성
---
+++ topic_modeling/tokenizer.py
@@ -0,0 +1,160 @@
+import asyncio
+import datetime
+import re
+from datetime import time
+from xml.etree.ElementTree import ParseError
+from hanspell import spell_checker
+from tqdm import tqdm
+from gensim import corpora
+from gensim.models import LdaModel, TfidfModel
+import warnings
+from eunjeon import Mecab
+
+warnings.filterwarnings("ignore")
+import os
+from gensim.models.wrappers import LdaMallet
+
+class Tokenizer:
+
+    def __init__(self, data, vocab_file):
+        self.data = data
+        self.vocab_file = vocab_file
+
+        pass
+
+    def date_condition(self, start_date="2005-01-01"):
+        self.data = self.data[self.data['date'] >= start_date].sort_values(by=['date']).reset_index(drop=True)[['date', 'text']]
+
+
+    # 네이버 맞춤법 체크 // 이후에 멀티스레드 등의 방식으로 진행해야함
+    def check_spell(self, data_trunc=False, max_char=500, dropna=True):
+        '''
+        :param df_text: 맞춤법 교정할 dataframe
+        :param max_char: 최대 교정가능 글자수 (500)
+        :return:
+        '''
+
+        async def pre_func(text):
+            # 띄어쓰기 전체 합침
+            pattern = re.compile(r'\s+')
+            sentence = re.sub(pattern, '', text).strip()
+
+            # 맞춤법을 위해 500자 이전 . 까지 concat // . 좌측이 숫자가 아니면 okay
+            for_ch = re.match(r".+[^0-9]\.", sentence[:max_char]).group()
+
+            try:
+                ch_text = spell_checker.check(for_ch).checked
+            except ParseError:
+                return None
+
+            if for_ch == ch_text:
+                return None
+
+            # 네이버 500자 맞춤법 교정
+            print("final : ", ch_text)
+            return ch_text
+
+        tqdm.pandas()
+        now = datetime.datetime.now()
+        if data_trunc > 0:
+            self.data['text'] = self.data['text'][:data_trunc].progress_apply(lambda x: asyncio.run(pre_func(x)))
+        else:
+            self.data['text'] = self.data['text'].progress_apply(lambda x: asyncio.run(pre_func(x)))
+        print(datetime.datetime.now()-now)
+        # now = datetime.datetime.now()
+        # if data_trunc > 0:
+        #     self.data['text'] = self.data['text'][:data_trunc].progress_apply(lambda x: pre_func(x))
+        # else:
+        #     self.data['text'] = self.data['text'].progress_apply(lambda x: pre_func(x))
+        # print(datetime.datetime.now()-now)
+
+        if dropna is True:
+            self.data = self.data.dropna().reset_index(drop=True)
+
+    def ex_stopword(self, bool):
+        allow_vv = ["NNG", "NNP", "NNB", "NR", "NP", "VV", "VA", "VX", "VCP", "VCN", "MM", "MAG", "MAJ"]
+        tqdm.pandas()
+        if bool is True:
+            mecab = Mecab()
+            self.data['stopped_text'] = self.data['text'].progress_apply(lambda x: " ".join([word[0] for word in mecab.pos(str(x)) if word[1] in allow_vv]))
+        else:
+            self.data['stopped_text'] = self.data['text']
+
+    def train_vocab(self, tokenizer, vocab_size=32000, min_freq=5):
+
+        self.data['stopped_text'].to_csv("topic_modeling/for_tokenize.txt", encoding='utf-8')
+
+        # Initialize a tokenizer
+        tokenizer = tokenizer
+
+        # Then train it!
+        tokenizer.train(
+            files=["topic_modeling/for_tokenize.txt"],
+            vocab_size=vocab_size,
+            min_frequency=min_freq,
+            show_progress=True,
+            )
+
+        # And finally save it somewhere
+        tokenizer.save_model(".", "topic_modeling/huggingface_tokenizer_kor_32000")
+
+    def tokenizer(self, tokenizer):
+        # print(tokenizer.encode(df['text'].tolist()).tokens)
+        # df['token'] = tokenizer.encode(df['text'].tolist()).tokens
+        tqdm.pandas()
+        self.data['token'] = self.data['stopped_text'].progress_apply(lambda x: tokenizer.encode(x).tokens)
+
+        # 토큰화 이후 ##제거
+        self._token_refine()
+        return self.data
+
+    def _token_refine(self):
+
+        print("체크용 프린트")
+        print(self.data.loc[3].token)
+
+        self.data['token'] = self.data['token'].progress_apply(lambda x: " ".join(x))
+
+
+        print(self.data.loc[3].token)
+
+
+        self.data['token'] = self.data['token'].str.replace(r'##[^\s]*[\s]', '')
+        self.data['token'] = self.data['token'].str.replace(r'\[[\w]{3}\]', '')
+        self.data['token'] = self.data['token'].progress_apply(lambda x: x.split())
+
+        print(self.data.loc[3].token)
+
+    def get_lda(self, n):
+        id2word = corpora.Dictionary(self.data['token'])
+        corpus_TDM = [id2word.doc2bow(doc) for doc in self.data['token']]
+        tfidf = TfidfModel(corpus_TDM)
+        corpus_TFIDF = tfidf[corpus_TDM]
+        lda = LdaModel(corpus=corpus_TFIDF,
+                            id2word=id2word,
+                            num_topics=n,
+                            random_state=100)
+
+        return lda.print_topics(n)
+
+    def mallet_lda(self, num=100):
+
+        id2word = corpora.Dictionary(self.data['token'])
+        texts = self.data['token']
+        corpus = [id2word.doc2bow(text) for text in texts]
+        os.environ['Mallet_HOME'] = 'C:\\Mallet'
+        mallet_path = 'C:\\Mallet\\bin\\mallet'
+        ldamallet = LdaMallet(mallet_path, corpus=corpus, num_topics=num, id2word=id2word)
+        return ldamallet.print_topics(num)
+
+    def get_lda_with_data(self, n, data):
+        id2word = corpora.Dictionary(data['token'])
+        corpus_TDM = [id2word.doc2bow(doc) for doc in data['token']]
+        tfidf = TfidfModel(corpus_TDM)
+        corpus_TFIDF = tfidf[corpus_TDM]
+        lda = LdaModel(corpus=corpus_TFIDF,
+                            id2word=id2word,
+                            num_topics=n,
+                            random_state=100)
+
+        return lda.print_topics(n)
